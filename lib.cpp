13497597c77ba83f06e6908435fbc9b5f701dc0a
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-12-08 16:30:23 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+bool conversione(char &a) {
+
+    if ((a >= 0) && (a <= 9)) {
+        return false;
+    } else
+    {
+        if(a >= 'A' && a <= 'Z'){
+            a= a + 32;
+            return true;
+        }
+        else if((a >='a')&&(c<='z')){
+            a = a-32;
+            return true;
+        }
+    } 
+}
