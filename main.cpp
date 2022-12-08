6bbb36621a968a4cf6ee04d7a112a56394872097
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-12-08 16:31:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+int main() {
+    char a;
+    bool b;
+cin>>a;
+b= conversione(a);
+if(t== true){
+    cout<<a<<endl;
+}
+else{
+    cout<< "errore" <<endl;
+}
+    return 0;
 }
